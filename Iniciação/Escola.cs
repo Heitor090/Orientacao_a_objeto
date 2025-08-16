fdf408d0af63f510f325bb50b0dc5d8bd4532e15
Heitor090 <heitor.bleao@senacsp.edu.br>
2025-08-15 22:09:44 -0300

    Add files via upload

---
+++ Iniciação/Escola.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"O aluno: {Nome} | Notas: {Nota1} e {Nota2}");
+
+        }
+
+        public double CalcularMedia()
+        {
+            double media;
+            return media = (Nota1 + Nota2) / 2;
+           
+        }
+
+    }
+
+}
